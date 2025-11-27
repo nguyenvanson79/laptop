8c917fc5949f173089f1c73f5a6019bea9e1b60d
Son Nguyen <nguyenson79v79@gmail.com>
2025-11-27 14:14:50 +0700

    okla

---
+++ app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express() ;
+
+const port = 8080 ;
+
+app.get("/", (req, res) => {
+    res.send ( "hello word")
+})
+app.get("/tieptheo", (req, res) => {
+    res.send ( "hello word")
+})
+
+
+app.listen(port , () => {
+    console.log("my app running " ,  port)
+})
